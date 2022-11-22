9ee2c8a0c4e3a98b4a43a18645c9cfca7121036d
Slothagami <slothagami@gmail.com>
2022-11-22 14:17:15 +1100

    test with multiple outputs

---
+++ neuralnetwork.py
@@ -0,0 +1,62 @@
+from activations import *
+import numpy as np 
+
+class NeuralNetwork:
+    def __init__(self, layers, /, learn_rate=.01):
+        self.layers = layers
+        self.lr = learn_rate
+
+        self.activations = []
+        self.weights = []
+        self.biases  = np.random.rand(len(layers))
+
+        for i, layer in enumerate(layers[:-1]):
+            self.weights.append( # +1 to account for bias node
+                np.random.rand(layer, layers[i + 1]) )
+
+    def predict(self, input):
+        self.activations = []
+        out = np.array(input)
+
+        for layer, bias in zip(self.weights, self.biases): 
+            out = np.dot(out, layer) + bias
+            self.activations.append(out)
+            out = sigmoid(out)
+
+        return out
+
+    def feed(self, input, target):
+        out = self.predict(input)
+
+        # Mean Squared Error
+        error = np.sum( np.square(out - target) )
+
+        # Error Prime
+        error_prime = 2 * (out - target)
+
+        for weight, activation, bias in zip(
+            reversed(self.weights), 
+            reversed(self.activations),
+            reversed(self.biases)
+        ):
+            weight -= error_prime * self.lr
+            bias   -= error_prime * self.lr
+            error_prime = np.dot((sigmoid_prime(activation) * activation).T, error_prime)
+
+
+        print(f"Error: {error:.4f} → {target}? → {out}")
+
+
+if __name__ == "__main__":
+    nn = NeuralNetwork((2, 2, 2), learn_rate=.1)
+    for n in range(10_000_000):
+        if n % 500_000 == 0:
+            nn.feed([1, 1], [1, 0])
+            nn.feed([1, 0], [1, 0])
+            nn.feed([0, 1], [1, 0])
+            nn.feed([0, 0], [0, 1])
+
+    print(nn.predict([1,1]))
+    print(nn.predict([1,0]))
+    print(nn.predict([0,1]))
+    print(nn.predict([0,0]))
