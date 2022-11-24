34dfc8091267019736d153de6cc367739e809f75
Slothagami <slothagami@gmail.com>
2022-11-25 01:06:20 +1100

    refactor mnist.py to be more readable

--- network/mnist.py
+++ network/mnist.py
@@ -8,29 +8,31 @@ import numpy as np
 
 (train_batch, train_labels), (test_batch, test_labels) = mnist.load_data()
 
-# Reshape Training Data
+# Reshape Data
 train_batch = train_batch.reshape(train_batch.shape[0], 1, 28*28).astype("float32")
+test_batch  = test_batch .reshape(test_batch .shape[0], 1, 28*28).astype("float32")
+
 train_batch /= 255 # normalize pixel values
+test_batch  /= 255
 
 train_labels = np_utils.to_categorical(train_labels) # reformat into vector labels
-
-# Reshape Testing Data
-test_batch = test_batch.reshape(test_batch.shape[0], 1, 28*28).astype("float32")
-test_batch /= 255 # normalize pixel values
-
-test_labels = np_utils.to_categorical(test_labels) # reformat into vector labels
-
+test_labels  = np_utils.to_categorical(test_labels )
 
 
 # Train
 nn = Network(lr=.1)
-
 nn.config((28*28, 100, 50, 10), tanh, tanh_prime)
 nn.train(test_batch, test_labels, 10)
 
-print("\nTest:")
+
+# Print Results
+print("\nRaw Output:")
 for sample in nn.predict(test_batch[:3]): 
     print(sample)
+
+print("\nRounded Output:")
+for sample in nn.predict(test_batch[:3]): 
+    print(np.abs(np.round(sample)))
 
 print("\nLabels:")
 for label in test_labels[:3]: 
