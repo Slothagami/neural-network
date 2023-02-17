f556e7a0e1c606bc684d7e973c9f61141ed3b7e8
Slothagami <slothagami@gmail.com>
2023-02-17 21:42:48 +1100

    remove gan.py

--- network/gan.py
+++
@@ -1,6 +0,0 @@
-from network.neuralnet import * 
-
-class GAN:
-    def __init__(self, generator: NeuralNet, discriminator: NeuralNet):
-        self.generator = generator
-        self.discriminator = discriminator
