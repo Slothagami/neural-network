a5509ce147e5ceb6a1279a47583990eb3c761e1b
Slothagami <slothagami@gmail.com>
2023-04-13 21:50:29 +1000

    finished FC network test

--- source/main.c
+++ source/main.c
@@ -8,23 +8,27 @@ int main() {
 	// make network (mnist) - lr=.01, ((28*28, 100, 50, 10), Tanh)
 	srand(time(NULL)); // set the random seed to the time
 
-	mat *input  = rand_matrix(2, 1);
 	mat *target = new_matrix(1, 1);
-	mfill(target, 0);
+	mfill(target, 1);
 
 	// Network test
 	unsigned int layers[] = {2, 3, 1};
 	int num_layers = sizeof(layers) / sizeof(unsigned int) - 1; // len(layers) - 1
 	Network* net = make_fc_network(layers, num_layers, mat_tanh, mat_tanh_grad, mse_grad);
 
-	// forward
-	mat* result = net_forward(net, input);
-	printm(result);
+	// training loop
+	mat* result;
+	mat* input;
+	for(int i = 0; i < 20; i++) {
+		// forward
+		input  = rand_matrix(2, 1);
+		result = net_forward(net, input);
 
-	// backward
-	double error = mse(target, result);
-	printf("Error: %f\n", error);
-	net_backward(net, input, result, target, mse_grad, .01);
+		// backward
+		double error = mse(target, result);
+		printf("Error: %f\n", error);
+		net_backward(net, input, result, target, mse_grad, .1);
+	}
 
 	free_network(net);
 	mfree(result);
