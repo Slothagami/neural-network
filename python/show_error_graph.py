b0b0154487c9c00a83486c6c581a53ac5082768b
Slothagami <slothagami@gmail.com>
2023-04-13 22:25:44 +1000

    fix graph display program

--- python/show_error_graph.py
+++ python/show_error_graph.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 from pyperclip import paste 
 
 lines = paste().strip().split("\n")
-numbers = [line.split(" ")[-1] for line in lines]
-numbers = [float(num) for num in numbers]
+numbers = [line.split(" ")[-1].strip() for line in lines]
+numbers = [float(num) for num in numbers if num != ""]
 
 plt.title("Training Error")
 plt.xlabel("Epoch")
