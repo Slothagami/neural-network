dcf1cba8a24531911a9f1d5df3c3f5c3669b627f
Slothagami <slothagami@gmail.com>
2023-04-21 21:03:12 +1000

    organized python module into Matrix object

--- python/network.py
+++ python/network.py
@@ -11,19 +11,22 @@ class Matrix(ctypes.Structure):
         ("size",   ctypes.c_uint)
     ]
 
+    @staticmethod
+    def from_numpy(np_array):
+        width, height = np_array.shape 
+        mat = new_matrix(width, height)
+        data = np_array.astype(np.float64).flatten()
+        mat.contents.data = data.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        return mat
+    
+    @staticmethod
+    def to_numpy(mat_ptr):
+        mat = mat_ptr.contents
+        data_ptr = ctypes.cast(mat.data, ctypes.POINTER(ctypes.c_double * mat.size))
+        data = np.ctypeslib.as_array(data_ptr.contents)
+        return np.reshape(data, (mat.height, mat.width))
+
+# C Function Definitions
 new_matrix = lib.new_matrix
 new_matrix.argtypes = [ctypes.c_uint, ctypes.c_uint]
 new_matrix.restype  =  ctypes.POINTER(Matrix)
-
-def mat_from_numpy(np_array):
-    width, height = np_array.shape 
-    mat = new_matrix(width, height)
-    data = np_array.astype(np.float64).flatten()
-    mat.contents.data = data.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-    return mat
-
-def mat_to_numpy(mat_ptr):
-    mat = mat_ptr.contents
-    data_ptr = ctypes.cast(mat.data, ctypes.POINTER(ctypes.c_double * mat.size))
-    data = np.ctypeslib.as_array(data_ptr.contents)
-    return np.reshape(data, (mat.height, mat.width))
--- python/test.py
+++ python/test.py
@@ -1,6 +1,7 @@
-from network import *
+from network import Matrix
+import numpy as np
 
 arr = np.zeros((3, 3))
-mat = mat_from_numpy(arr)
-arr = mat_to_numpy(mat)
+mat = Matrix.from_numpy(arr)
+arr = Matrix.to_numpy(mat)
 print(arr)
