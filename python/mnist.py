2df1590d3af89b1e8c385622fb571741c8123110
Slothagami <slothagami@gmail.com>
2023-05-10 22:36:21 +1000

    temporarily limiting the number of training samples

--- python/mnist.py
+++ python/mnist.py
@@ -5,17 +5,18 @@ print("Loading Data...")
 from keras.datasets import mnist
 from keras.utils import np_utils
 
+n_test = 10
 (train_batch, train_labels), (test_batch, test_labels) = mnist.load_data()
 
 # Reshape Data
-train_batch = train_batch.reshape(train_batch.shape[0], 1, 28*28).astype("float32")
-test_batch  = test_batch .reshape(test_batch .shape[0], 1, 28*28).astype("float32")
+train_batch = train_batch.reshape(train_batch.shape[0], 1, 28*28).astype("float32")[:n_test]
+test_batch  = test_batch .reshape(test_batch .shape[0], 1, 28*28).astype("float32")[:n_test]
 
 train_batch /= 255 # normalize pixel values
 test_batch  /= 255
 
-train_labels = np_utils.to_categorical(train_labels) # reformat to vector labels
-test_labels  = np_utils.to_categorical(test_labels )
+train_labels = np_utils.to_categorical(train_labels)[:n_test] # reformat to vector labels
+test_labels  = np_utils.to_categorical(test_labels )[:n_test]
 print("Data Loaded.")
 
 nn = Net(mse_grad, mse)
@@ -25,8 +26,7 @@ nn.add(TanhLayer())
 nn.add(FCLayer(100, 50))
 nn.add(TanhLayer())
 nn.add(FCLayer(50, 10))
-nn.add(TanhLayer())
 
-nn.train(train_batch, train_labels, 5, .01, 100, 100)
+nn.train(train_batch, train_labels, 5, .01, 1, 1, test_batch, test_labels)
 
 # # Gets ~80% acc w/ 5 epochs
